8a639e08679de9c337c1280f1cf8e7a2ad33fd29
Abhinav <abhinavsinghhg@gmail.com>
2023-09-19 17:48:49 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { data } from "./data";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-base mt-3 p-1 flex flex-col items-center">
+      {/* SearchBar */}
+      <div className="mb-2 border rounded-sm">
+        <input
+          type="text"
+          placeholder="Search Animal Here..."
+          onChange={(e) => setSearch(e.target.value)}
+          className=" border-stone-800 p-2 w-72 text-sm"
+        />
+      </div>
+
+      {/* List of Items */}
+      <table className="table-auto border-collapse w-8/12">
+        <thead>
+          <tr className="bg-gray-200 border-2 border-black">
+            <th className="py-2 px-3 text-center border border-gray-500 w-1/6">
+              S.No.
+            </th>
+            <th className="py-2 px-3 text-center border border-gray-500 w-2/5">
+              Animal Name
+            </th>
+            <th className="py-2 px-3 text-center border border-gray-500">
+              Scientific Name
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {data
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.animal_name.toLowerCase().includes(search);
+            })
+            .map((item) => (
+              <tr key={item.id}>
+                <td className="py-2 px-3 text-center border border-gray-500">
+                  {item.id}
+                </td>
+                <td className="py-2 px-3 text-center border border-gray-500">
+                  {item.animal_name}
+                </td>
+                <td className="py-2 px-3 text-center border border-gray-500">
+                  {item.animal_scientific}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 }
